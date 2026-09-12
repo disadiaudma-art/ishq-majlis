7ac300a59db06c5ff90db4423dc0b5b31cbb52f3
disadiaudma-art <disadiaudma@gmail.com>
2026-09-12 16:14:26 +0530

    Route has updated

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<RegistrationPage />} />
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/go-to/admin" element={<AdminPage />} />
+        <Route path="/go-to/admin/access" element={<AdminPage />} />
       </Routes>
     </HashRouter>
   )
